2c115555ba5fef184c0da848049e0827fab54a2e
Matthew Eddy <mattheweddy@Matthews-Air.attlocal.net>
2021-04-28 10:00:21 -0500

    added default route for server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
 
+app.get('/', (req, res) => {
+    res.send('data');
+})
+
 app.get("/health", (req, res, next) => {
   res.status(200).send("Hello!");
 });
